1b30709a9cd035102951d03027c387e9f98c50b0
Jamstruth <jamstruth18@gmail.com>
2024-12-04 21:01:33 +0000

    Advent of Code Day 1

---
+++ day_1/day1.py
@@ -0,0 +1,30 @@
+
+def parse_input_file():
+  lines = []
+  with open("day1_input", "r") as input:
+    lines = input.readlines()
+  left_list = []
+  right_list = []
+  for line in lines:
+    [left_num, right_num] = line.split('   ')
+    left_list.append(int(left_num))
+    right_list.append(int(right_num))
+  return left_list, right_list
+
+left_list, right_list = parse_input_file()
+print(f"Left List Length: {len(left_list)} Right List Length: {len(right_list)}")
+left_list.sort()
+right_list.sort()
+
+total_difference = 0
+for left, right in zip(left_list, right_list):
+  total_difference += (abs(left - right))
+print(f"Total Difference between Left+Right: {total_difference}")
+
+similarity_score = 0
+for left in left_list:
+  for right in right_list:
+    if left == right:
+      similarity_score += left
+
+print(f"Similarity Score: {similarity_score}")
