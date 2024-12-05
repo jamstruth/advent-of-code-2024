282dd92c4d19cd79428b950f9b786ed1ce817a23
Jamstruth <jamstruth18@gmail.com>
2024-12-05 23:46:52 +0000

    Adding Down-left test

--- day4.py
+++ day4.py
@@ -2,8 +2,7 @@ from pprint import pprint
 
 INPUT_FILE = "inputs/day4_input"
 XMAS = 'XMAS'
-SAMX = XMAS[::-1]
-step_value = len(XMAS)-1
+SAMX = 'SAMX'
 
 # Then lets load this into a 2D array
 character_matrix = []
@@ -14,19 +13,22 @@ with open(INPUT_FILE, "r") as input:
 # Now we start searching
 total_xmas = 0
 
+step_value = len(XMAS)-1 # Step value stops us going off the side for down + down-right directions
 for row in range(len(character_matrix)-step_value):
   for col in range(len(character_matrix[0])-step_value):
     # Build Strings
     right_string = ""
     down_string = ""
     down_right_string = ""
+    down_left_string = ""
     for offset in range(len(XMAS)):
       right_string += character_matrix[row][col+offset]
       down_string += character_matrix[row+offset][col]
       down_right_string += character_matrix[row+offset][col+offset]
-    all_strings = [right_string,down_string,down_right_string]
+      if(col-offset > 0):
+        down_left_string += character_matrix[row+offset][col-offset]
+    all_strings = [right_string,down_string,down_right_string,down_left_string]
     for string in all_strings:
-      print(string)
       if string == XMAS or string == SAMX:
         total_xmas += 1
 
