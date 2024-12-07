23749f0d30cc95e2c0662a3a0ec9f2b88eb28317
Jamstruth <jamstruth18@gmail.com>
2024-12-07 00:01:12 +0000

    Intitial attempts at D1P2 - Loop when obstacle placed at 1,8 in the practice data but not for getting back to the start position

--- day6.py
+++ day6.py
@@ -2,8 +2,9 @@ from pprint import pprint
 from enum import Enum
 
 class GuardStatus(Enum):
-  ON_MAP = 1
+  NORMAL = 1
   OFF_MAP = 2
+  LOOPED = 3
 
 class Direction(Enum):
   NORTH = 1
@@ -33,27 +34,36 @@ class Map:
 
 class Guard:
   def __init__(self, direction: Direction, location: tuple, map: Map):
+    self.start_location = location
+    self.start_direction = direction
     self.direction = direction
     self.location = location
-    self.status = GuardStatus.ON_MAP
+    self.status = GuardStatus.NORMAL
     self.map = map
 
+  def check_has_looped(self):
+     return self.location == self.start_location and self.direction == self.start_direction
+
   def move(self):
-    target_location = self.determine_target_move()
-    print(f"Trying to move to {target_location}")
-    if (self.map.is_coord_on_map(target_location)):
-      if (self.map.is_coord_obstacle(target_location)):
-        # If there's an obstacle we need to turn and try again
-        print(f"{target_location} is obstacle, turning and retrying")
-        self.turn_right()
-        return self.move()
+    if(self.status == GuardStatus.NORMAL):
+      target_location = self.determine_target_move()
+      if (self.map.is_coord_on_map(target_location)):
+        self.perform_move(target_location)
+        if self.check_has_looped():
+          print('Guard looped!')
+          self.status = GuardStatus.LOOPED
       else:
-        print(f"Moving to {target_location}")
-        self.location = target_location
+        print('Moving Off Map')
+        self.status = GuardStatus.OFF_MAP
+        self.location = None
+
+  def perform_move(self, target_location):
+    if (self.map.is_coord_obstacle(target_location)):
+      # If there's an obstacle we need to turn and try again
+      self.turn_right()
+      return self.move()
     else:
-      print('Moving Off Map')
-      self.status = GuardStatus.OFF_MAP
-      self.location = None
+      self.location = target_location
 
   def determine_target_move(self):
     match self.direction:
@@ -102,7 +112,6 @@ def parse_input_to_guard(input_file):
       for x_coord, char in enumerate(line):
         if char == '#':
           # Build Obstacle
-          print((x_coord, y_coord))
           obstacles.append(Obstacle(location=(x_coord, y_coord)))
           continue
         if char in guard_symbols:
@@ -113,24 +122,35 @@ def parse_input_to_guard(input_file):
     return Guard(direction=guard_dir, location=(guard_x, guard_y), map=map)
 
 
-INPUT_FILE = 'inputs/day6_input'
+INPUT_FILE = 'inputs/day6_practice'
 guard = parse_input_to_guard(INPUT_FILE)
 guard_locations = {guard.location}
 
-print(f"There is an obstacle at (4,0): {guard.map.is_coord_obstacle((4, 0))}")
-print(f"(-1, 0) is on map: {guard.map.is_coord_on_map((-1, 0))}")
-print(f"(0, -1) is on map: {guard.map.is_coord_on_map((0, -1))}")
-(x_dim, y_dim) = guard.map.dimensions
-print(f"({x_dim}, 0) is on map: {guard.map.is_coord_on_map((x_dim, 0))}")
-print(f"(0, {y_dim}) is on map: {guard.map.is_coord_on_map((0, y_dim))}")
-print(f"(1, 5) is on map: {guard.map.is_coord_on_map((1, 5))}")
-
 # Move guard until his status is OFF_MAP
 # Note his movements
-while(guard.status == GuardStatus.ON_MAP):
+while(guard.status != GuardStatus.OFF_MAP):
   print(f"Guard at: {guard.location}")
   guard.move()
   if(guard.location != None):
     guard_locations.add(guard.location)
-
 print(f"Guard Visited {len(guard_locations)} unique locations")
+
+# Simulate lots of guards adding a new object in each location and count how many cause a loop
+initial_guard = parse_input_to_guard(INPUT_FILE)
+loop_count = 0
+(x_dim, y_dim) = initial_guard.map.dimensions
+for x in range(x_dim):
+  for y in range(y_dim):
+    print(f"Trying obstacle at {(x, y)}")
+    if(initial_guard.location == (x, y) or initial_guard.map.is_coord_obstacle((x, y))):
+      continue # Skip any existing obstacles and the guard
+    new_obstacles = [Obstacle(location=(x, y))]
+    curr_map = Map(dimensions=initial_guard.map.dimensions, obstacles=initial_guard.map.obstacles[:]+new_obstacles)
+    curr_guard = Guard(direction=initial_guard.direction, location=initial_guard.location, map=curr_map)
+    while(curr_guard.status == GuardStatus.NORMAL):
+      # print(f"Current Guard at: {curr_guard.location}")
+      # print(f"Current Guard status: {curr_guard.status}")
+      curr_guard.move()
+    if (curr_guard.status == GuardStatus.LOOPED):
+      loop_count += 1
+print(f"There are {loop_count} obstacle locations that cause a loop")
